7a3d43a37fc1cf7a7321a2576378bcc08c112fd0
Taylor Flatt <taylorjflatt@gmail.com>
2017-06-01 19:55:14 -0500

    Update bot.js
    Added a few more sayings.

--- bot.js
+++ bot.js
@@ -8,12 +8,25 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-  if (message.content.startsWith('!cjface')) {
+  if (message.content.startsWith('!cjface') || message.content.startsWith('!facts')) {
       var facts = {
           1: "Did you know that 2ng/3ng and 3ng/4ng are the same thing?",
-          2: "That guy was buffed out the DICK!"
+          2: "That guy was buffed out the DICK!",
+          3: "Pasgetti >:[",
+          4: "Your retarded",
+          5: "Why is my guy keep stopping?",
+          6: "CFG....Country Fried Chicken",
+          7: "I pooped my ass",
+          8: "I can spell escape! E-s-c-a-p-y. I added a y.",
+          9: "I'm going to find everyone I am >:[",
+          10: "I'm the least wanting to get up person.",
+          11: "Dude, make it your facepage.",
+          12: "How he didn't die is beyond me.",
+          13: "That guy died of death",
+          14: "THE FUCKING VAN DOESN'T MAKE SENSE!!",
+          15: "I definition didn't shit my pants at hardees...twice..."
       };
-      var rand = randomInt(1, 2);
+      var rand = randomInt(1, Object.keys(facts).length);
     message.channel.send(facts[rand]);
   }
 });
